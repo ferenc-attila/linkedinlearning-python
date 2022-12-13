ca436d68809358a1abb8dbf4ec773509bd23db6c
ferenc-attila <attila.ferenc.80@gmail.com>
2022-12-13 21:31:16 +0100

    classes, constructors, inheritance

---
+++ Ch2 - Basics/classes.py
@@ -0,0 +1,36 @@
+#
+# Example file for working with classes
+# LinkedIn Learning Python course by Joe Marini
+#
+
+class Vehicle():
+    def __init__(self, bodystyle):
+        self.bodystyle = bodystyle
+
+
+class Car(Vehicle):
+    def __init__(self, enginetype):
+        super().__init__("Car")
+        self.wheels = 4
+        self.doors = 4
+        self.enginetype = enginetype
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, enginetype, hassidecar):
+        super().__init__("Motorcycle")
+        if (hassidecar):
+            self.wheels = 3
+        else:
+            self.wheels = 2
+        self.doors = 0
+        self.enginetype = enginetype
+
+
+car1 = Car("gas")
+car2 = Car("electric")
+mc1 = Motorcycle("gas", True)
+
+print(mc1.wheels)
+print(car1.enginetype)
+print(car2.doors)
