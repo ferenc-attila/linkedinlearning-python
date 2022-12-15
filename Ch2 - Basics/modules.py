eb1720cdd8b95b7b7f90447a21c508f83960fb71
ferenc-attila <attila.ferenc.80@gmail.com>
2022-12-15 14:38:29 +0100

    import and use modules

---
+++ Ch2 - Basics/modules.py
@@ -0,0 +1,17 @@
+# LinkedIn Learning Python course by Joe Marini
+#
+
+# TODO: import the math module, which contains features for working with mathematics
+import math
+
+# TODO: the math module contains lots of pre-built functions
+print("The square root of 16 is", math.sqrt(16))
+
+# TODO: in addition to functions, some modules contain useful constants 
+print("PI is ",  math.pi)
+
+# TODO: try some of the math functions for yourself here:
+print(math.floor(12.4))
+print(math.floor(12.9))
+print(math.ceil(12.4))
+print(math.ceil(12.9))
