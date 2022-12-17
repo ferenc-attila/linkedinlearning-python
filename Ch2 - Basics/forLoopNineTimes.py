20723b7fb65372ee0cc7b2cf694791e1a4f6e367
ferenc-attila <attila.ferenc.80@gmail.com>
2022-12-17 19:38:03 +0100

    I think there is an error in the test

---
+++ Ch2 - Basics/forLoopNineTimes.py
@@ -0,0 +1,9 @@
+# Question: What is the proper code for creating a "for" loop that will execute 9 times starting at the number 6?
+
+# Answer by me:
+for i in range(6, 15):
+    print(i)
+
+# Correct answer by LinkedIn Learning:
+for i in range(6, 16):
+    print(i)
