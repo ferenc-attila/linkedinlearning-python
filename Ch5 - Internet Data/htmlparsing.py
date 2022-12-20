18d4dfbb00b929284b9d37bc6fc595727b5f9043
ferenc-attila <attila.ferenc.80@gmail.com>
2022-12-20 16:31:14 +0100

    html parsing solved

---
+++ Ch5 - Internet Data/htmlparsing.py
@@ -0,0 +1,53 @@
+# 
+# Example file for parsing and processing HTML
+# LinkedIn Learning Python course by Joe Marini
+#
+
+from html.parser import HTMLParser
+
+paragraphs = 0
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        print("Encountered a comment:", data)
+        pos = self.getpos()
+        print("At line:", pos[0], " position", pos[1])
+
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+        pos = self.getpos()
+        print("At line:", pos[0], " position", pos[1])
+
+        global paragraphs
+        if tag == "p":
+            paragraphs += 1
+
+        if len(attrs) > 0:
+            print("Attributes:")
+            for attribute in attrs:
+                print("\t", attribute[0], "=", attribute[1])
+
+    def handle_data(self, data):
+        if data.isspace():
+            return
+        print("Encountered a text data:", data)
+        pos = self.getpos()
+        print("At line:", pos[0], " position", pos[1])
+
+
+def main():
+    # instantiate the parser and feed it some HTML
+    parser = MyHTMLParser()
+    
+    f = open("samplehtml.html")
+    if f.mode == "r":
+        contents = f.read() # read the entire file
+        parser.feed(contents)    
+
+    print("Paragraph tags:", paragraphs)
+
+
+if __name__ == "__main__":
+    main()
+  
