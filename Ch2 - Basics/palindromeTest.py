f71e5364282784cc8f2e9ea744b7d12df546f199
ferenc-attila <attila.ferenc.80@gmail.com>
2022-12-21 09:06:20 +0100

    palindrome test refactored

--- Ch2 - Basics/palindromeTest.py
+++ Ch2 - Basics/palindromeTest.py
@@ -1,24 +1,28 @@
-result = True
+def clean_text(string_to_test):
+    string_to_test = string_to_test.lower()
+    for char in string_to_test:
+        if not char.isalnum():
+            string_to_test = string_to_test.replace(char, "")
+    return string_to_test
 
-# Is there any nicer solution?
-stringToTest = ""
 
-while stringToTest != "exit":
-    stringToTest = input("Enter a string to test for palindrome or 'exit':")
-    stringToTest = stringToTest.lower()
-    for character in stringToTest:
-        if not character.isalnum():
-            stringToTest = stringToTest.replace(character, "")
+def is_palindrome(string_to_test):
+    for index_of_character in range(0, (string_to_test.__len__() / 2).__ceil__()):
+        print(string_to_test[index_of_character], "vs.", string_to_test[(index_of_character+1)*-1])
+        if string_to_test[index_of_character] != string_to_test[(index_of_character + 1) * -1]:
+            return False
+    return True
 
-    print(stringToTest)
 
-    if stringToTest == "exit":
+def get_text_from_user():
+    string_to_test = input("Enter a string to test for palindrome or 'exit':")
+    if string_to_test == "exit":
         print("Thanks for using this software. Good bye!")
         raise SystemExit
+    return string_to_test
 
-    for indexOfCharacter in range(0, (stringToTest.__len__() / 2).__ceil__()):
-        print(stringToTest[indexOfCharacter], "vs.", stringToTest[(indexOfCharacter+1)*-1])
-        if stringToTest[indexOfCharacter] != stringToTest[(indexOfCharacter + 1) * -1]:
-            result = False
 
-    print(result)
+stringToTest = get_text_from_user()
+while stringToTest != "exit":
+    stringToTest = clean_text(stringToTest)
+    print(is_palindrome(stringToTest))
