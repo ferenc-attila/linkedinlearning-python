d58300ed98a84cb6b183a501a13e6c1653a0eebb
ferenc-attila <attila.ferenc.80@gmail.com>
2022-12-21 09:19:58 +0100

    infinite while loop bug fixed cleaning

--- Ch2 - Basics/palindromeTest.py
+++ Ch2 - Basics/palindromeTest.py
@@ -15,8 +15,8 @@ def is_palindrome(string_to_test):
 
 
 def get_text_from_user():
-    string_to_test = input("Enter a string to test for palindrome or 'exit':")
-    if string_to_test == "exit":
+    string_to_test = input("Enter a string to test for palindrome or 'exit': ")
+    if clean_text(string_to_test) == "exit":
         print("Thanks for using this software. Good bye!")
         raise SystemExit
     return string_to_test
@@ -25,4 +25,5 @@ def get_text_from_user():
 stringToTest = get_text_from_user()
 while stringToTest != "exit":
     stringToTest = clean_text(stringToTest)
-    print(is_palindrome(stringToTest))
+    print("The result of palindrome test for text '", stringToTest, "': ", is_palindrome(stringToTest))
+    stringToTest = get_text_from_user()
